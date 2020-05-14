35604b89afebf0bc3cc71db7b60927618704872d
leodine <64403877+leodine@users.noreply.github.com>
2020-05-14 08:33:31 +0200

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+	$nameMovie = "Indiana Jones and the Last Crusade";
+	$alreadySeen = true;
+	$releaseYear = 1989;
+	$noteImdb = 8.2;
+	
+	echo "$nameMovie" . "<br>" . PHP_EOL;
+	echo "$alreadySeen" . "<br>" . PHP_EOL;
+	echo "$releaseYear" . "<br>" . PHP_EOL;
+	echo "$noteImdb" . "<br>" . PHP_EOL;
+?>
