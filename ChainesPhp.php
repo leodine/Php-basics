4ae43996d13a333437b553dea57cbb0ed3030d32
leodine <64403877+leodine@users.noreply.github.com>
2020-05-18 09:49:33 +0200

    Update ChainesPhp.php

--- ChainesPhp.php
+++ ChainesPhp.php
@@ -1,20 +1,26 @@
 <?php
 
-	function extractText(string $txt) : string
-	{
-	    $lengthMess1 = strlen($txt);
-	    $chiffreCle = $lengthMess1 / 2;
-	    $sousChaine = substr($txt, 5, $chiffreCle); //à partir du 6ème caractère
-	    $sousChaine = str_replace('@#', ' ', $sousChaine);
-	    return $sousChaine;
-	}
 
-	$message1 = '0@sn9sirppa@#?ia\'jgtvryko1';
-    	$message2 = 'q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj';
-    	$message3 = 'aopi?sgnirts@#?sedhtg+p9l!';
+function extractText(string $txt): string
+{
+    $lengthMess1 = strlen($txt);
+    $chiffreCle = $lengthMess1 / 2;
+    $sousChaine = substr($txt, 5, $chiffreCle); //à partir du 6ème caractère
+    $sousChaine = str_replace('@#?', ' ', $sousChaine);
 
-	extractText($message1);
-	extractText($message2);
-	extractText($message3);
+    /////ajout de la fonction native strrev
+    $txtSecret = strrev($sousChaine);
+
+    return $txtSecret;
+}
+
+$message1 = '0@sn9sirppa@#?ia\'jgtvryko1';
+$message2 = 'q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj';
+$message3 = 'aopi?sgnirts@#?sedhtg+p9l!';
+
+echo extractText($message1) . PHP_EOL;
+echo extractText($message2) . PHP_EOL;
+echo extractText($message3);
+
 
 ?>
